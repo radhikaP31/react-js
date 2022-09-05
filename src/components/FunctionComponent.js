8413fa5216688a86f2e27beb0ea0f088872ef9bc
Radhika Panchasara <radhika.p@int.biztechcs.com>
2022-09-05 11:30:55 +0530

    added: functional and class components

---
+++ src/components/FunctionComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//Functional syntax
+// function FunctionComponent() {
+//     return <h1>Hello Everyone, This is functional component example..</h1>
+// }
+
+//arrow syntax as ES6 coding standard
+const FunctionComponent = () => <h1>Hello Everyone, This is Functional component example..</h1>
+
+
+
+export default FunctionComponent;
--- src/components/Greet.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function welcome() {
-    return <h1>Hello Radhika</h1>
-}
-
-export default Greet
