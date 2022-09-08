710875d698fc2afad42411536f664a71bfcadce8
Radhika Panchasara <radhika.p@int.biztechcs.com>
2022-09-08 16:11:56 +0530

    complete: home,products, single product and cart pages with add to cart button, increment and decrement feature with total price and total quantity in cart page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import About from './pages/About';
+import ProductsPage from './pages/ProductsPage';
+import Cart from './pages/Cart';
+import SingleProduct from './pages/SingleProduct';
+import { Cartcontext } from './Cartcontext';
+import { getCart, storeCart } from './helpers';
 
 function App() {
+  const [cart, setCart] = useState({});
+  //fetch from local storage
+  useEffect(() => {
+
+    getCart().then(cart => {
+      if (Object.keys(JSON.parse(cart)).length != 0) {//check object length then setCart
+        setCart(JSON.parse(cart));
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    storeCart(JSON.stringify(cart));
+  }, [cart])
+
   return (
-    <div className="App">
-      Hello from React JS
+    <div>
+      <Router>
+        <Cartcontext.Provider value={{ cart, setCart }}>
+        <Navigation/>
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          {/* <Route exact path='/about' element={<About />}></Route> */}
+          <Route exact path='/products' element={<ProductsPage />}></Route>
+          <Route exact path="/products/:_id" element={<SingleProduct />}></Route>
+          <Route exact path='/cart' element={<Cart />}></Route>
+        </Routes>
+        </Cartcontext.Provider>
+      </Router>
     </div>
   );
 }
