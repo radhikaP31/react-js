d5b981672d1e5b9768c1707d6e54462242abcfd4
Radhika Panchasara <radhika.p@int.biztechcs.com>
2022-09-09 11:02:58 +0530

    update: improve file structure and remove unused files

--- src/components/ClassComponent.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class ClassComponent extends Component{
-    render() {
-        return <h1>Hello {this.props.heroName } a.k.a { this.props.name }, This is Class component example with props..</h1>
-    }
-}
-
-export default ClassComponent;
